0c89803c216a1570230298ce4cfa88851a3cc53a
Caio Fábio <caiofabio893@gmail.com>
2025-09-10 17:56:25 -0300

    DTO

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,16 +1,17 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.services.UserService;
 
 @RestController
@@ -20,11 +21,27 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	
+	// criaçao de uma classe dto para so retornar as infromaçoes necessarias 
+	// passa a lista de todos os usuarios
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() { 
-	List<User> list = service.findall();
-	return ResponseEntity.ok().body(list);  
+	public ResponseEntity<List<UserDTO>> findAll() { 
+		List<User> list = service.findall();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);  
+			
+		}
+	
+	
+	// retorna os usuarios pelo id 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findbyId(@PathVariable String id) { 
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));  
 		
 	}
+	
+	
+	
 	
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,8 +16,15 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 	
+	// retorna todos os usuarios 
 	public List<User> findall(){
 		return repo.findAll();
 	}
+	
+	// retorna so pelo id 
+	public User findById(String id) { 
+		 Optional<User> obj = repo.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	} 
 
 }
